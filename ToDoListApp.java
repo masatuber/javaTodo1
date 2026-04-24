f3be209f6b808f5c74d0829ccca57c32b2681935
masatuber <uvervivid1231@icloud.com>
2026-04-24 20:14:52 +0900

    scannerクラスまで書いた

---
+++ ToDoListApp.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToDoListApp {
+  public static void main(String[] args) {
+    // ArrayList<参照型名> 変数名 = new ArrayList<参照型名>(); https://qiita.com/ryome/items/cc01a7b6914e5b8b1579
+    ArrayList<String> tasks = new ArrayList<>();
+
+    // Javaでコンソールからのユーザー入力を受け取る場合やファイルからの入力を扱う場合 https://qiita.com/EasyCoder/items/f8b1a62a9298231efd21
+    Scanner scanner = new Scanner(System.in);
+  }
+}
